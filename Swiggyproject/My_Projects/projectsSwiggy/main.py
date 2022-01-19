bf2528ddb92ce71455c6a4980a1f1eb961ada4c8
Sudhakar Swain <91896763+sudhakar-swain-au27@users.noreply.github.com>
2022-01-19 20:49:27 +0530

    Add files via upload

---
+++ Swiggyproject/My_Projects/projectsSwiggy/main.py
@@ -0,0 +1,22 @@
+from entities.restaurant import Restaurant
+from entities.customer import Customer
+
+
+def run():
+    user_type = input(
+        "Are you a restaurant(R) registering or a customer buying food(C)")
+    if user_type == "R":
+        rest = Restaurant()
+        rest.register()
+        print(rest.dict_of_rests)
+
+    elif user_type == "C":
+        cust = Customer()
+        cust.order()
+    else:
+        print("Entered wrong choice, provide either R or C")
+
+
+if __name__ == "__main__":
+    run()
+
